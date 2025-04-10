7427e0ae5766d1144900bac8b31827ae1af83cb4
Dean-Cottrell <deanashton294@gmail.com>
2025-04-10 09:27:51 -0400

    Initial commit

---
+++ workbook-1/shoppinglist/src/test/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+    System.out.print("Oranges, apples, cranberries, blueberries, burgers, water, hot sauce, pot roast, beef jerky, orange chicken");
+    }
+}
